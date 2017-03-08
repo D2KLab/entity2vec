aef8af512a78bdc3e02c4dd8ec0bbda3da2839cf
3cixty <cixty@garoupe.eurecom.fr>
2017-03-08 11:24:25 +0100

    change bash variable increment

--- feature_generator.sh
+++ feature_generator.sh
@@ -3,10 +3,10 @@ for file in `ls datasets/movielens_1m/ratings_dbpedia_train_chunks`;
 	
 	do
 
-	 echo datasets/movielens_1m/ratings_dbpedia_train_chunks/$file;
-	 
-	 python -u lib/feature_generator.py -d movielens_1m -e num500_p1_q4_l10_d500.emd --output features/movielens_1m/global_p1_q4/train/training_chunk_$c.svm --training datasets/movielens_1m/ratings_dbpedia_train_chunks/$file &
-	
-	((c=c+1))
+	echo datasets/movielens_1m/ratings_dbpedia_train_chunks/$file;
+	#echo $c;
+	python -u lib/feature_generator.py -d movielens_1m -e num500_p1_q4_l10_d500.emd --output features/movielens_1m/global_p1_q4/train/training_chunk_$c.svm --training datasets/movielens_1m/ratings_dbpedia_train_chunks/$file &
+	c=$((c+1));
+
 	done
 
