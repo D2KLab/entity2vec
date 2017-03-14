30e0143dd4952d41a62bc763ab0f4ad3b35aa96d
3cixty <cixty@garoupe.eurecom.fr>
2017-03-14 12:07:12 +0100

    modified parameters and introduced wait for final concatenation

--- feature_generator.sh
+++ feature_generator.sh
@@ -1,12 +1,20 @@
 c=0;
-for file in `ls datasets/movielens_1m/ratings_dbpedia_train_chunks`;
+#sleep 8h;
+experiment='movielens_1m';
+dataset='ratings_dbpedia_relplusn_val_chunks';
+folder='datasets/'$experiment'/'$dataset'/';
+
+for file in `ls $folder`;
 	
 	do
 
-	echo datasets/movielens_1m/ratings_dbpedia_train_chunks/$file;
+	echo $folder$file;
 	#echo $c;
-	python -u lib/feature_generator.py -d movielens_1m -e num500_p1_q4_l10_d500.emd --output features/movielens_1m/global_p1_q4/train/training_chunk_$c.svm --training datasets/movielens_1m/ratings_dbpedia_train_chunks/$file &
+	python -u lib/feature_generator.py -d $experiment -e num500_p1_q1_l10_d500.emd --output features/$experiment/global_p1_q1/val/val_chunk_$c.svm --training 'datasets/'$experiment'/ratings_dbpedia_train.dat'  --test $folder$file &
 	c=$((c+1));
 
 	done
 
+wait
+
+cat features/$experiment/global_p1_q1/val/val_chunk* > features/$experiment/global_p1_q1/val/training_global_p1_q1.svm;
