611f3c06b68ed4bec0383cd18e437c13be369be9
3cixty <cixty@garoupe.eurecom.fr>
2017-03-26 17:43:17 +0200

    fix summary results.txt generation

--- ranking/ranking_script_feature_selection.sh
+++ ranking/ranking_script_feature_selection.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-p=4;
-q=1;
+p=1;
+q=4;
+
+rm results/global_p$p"_"q$q/test/summary_results.txt;
 
 for metric in P@1 P@5 P@10 MAP;
 	do
-	for model in lambda_mart ada_rank;
+	#for model in lambda_mart ada_rank;
+	for model in lambda_mart;
 		do
 		for feature in `ls feature_selection`;
 			do
@@ -15,26 +18,26 @@ for metric in P@1 P@5 P@10 MAP;
 
 				echo $feature;
 
-				file_model=models/global_p$p"_"q$q/$model"_"$feature"_"$metric.mdl;
+				file_model=models/global_p$p"_"q$q/test/$model"_"$feature"_"$metric.mdl;
+
 				feature_file=feature_selection/$feature.txt;
+
 				if [ $model = ada_rank ]; then
 					ranker=3;
 				else
 					ranker=6;
 				fi
 
-				echo "java -jar RankLib-2.1-patched.jar -train ../features/movielens_1m/global_p$p"_"q$q/train/training_global_p$p"_"q$q.svm -ranker $ranker -metric2t $metric -tvs 0.9 -test ../features/movielens_1m/global_p$p"_"q$q/val/val_global_p$p"_"q$q.svm -save $file_model -feature $feature_file > results/global_p$p"_"q$q/val/$model"_"$feature"_"$metric.out";
+				echo "java -jar RankLib-2.1-patched.jar -train ../features/movielens_1m/global_p$p"_"q$q/train/training_global_p$p"_"q$q.svm -ranker $ranker -metric2t $metric -tvs 0.9 -test ../features/movielens_1m/global_p$p"_"q$q/test/test_global_p$p"_"q$q.svm -save $file_model -feature $feature_file > results/global_p$p"_"q$q/test/$model"_"$feature"_"$metric.out;";
 				
-				java -jar RankLib-2.1-patched.jar -train ../features/movielens_1m/global_p$p"_"q$q/train/training_global_p$p"_"q$q.svm -ranker $ranker -metric2t $metric -tvs 0.9 -test ../features/movielens_1m/global_p$p"_"q$q/val/val_global_p$p"_"q$q.svm -save $file_model -feature $feature_file > results/global_p$p"_"q$q/val/$model"_"$feature"_"$metric.out;
+				java -jar RankLib-2.1-patched.jar -train ../features/movielens_1m/global_p$p"_"q$q/train/training_global_p$p"_"q$q.svm -ranker $ranker -metric2t $metric -tvs 0.9 -test ../features/movielens_1m/global_p$p"_"q$q/test/test_global_p$p"_"q$q.svm -save $file_model -feature $feature_file > results/global_p$p"_"q$q/test/$model"_"$feature"_"$metric.out;
 			
 
 				score=`tail results/global_p$p"_"q$q/val/$model"_"$feature"_"$metric.out | grep "on test data" | sed 's/$metric on test data: //g'`;
 
 				model_and_score=$model"_"$feature"_"$metric" $score";
 
-				rm results/global_p$p"_"q$q/val/summary_results.txt;
-
-				echo $model_and_score >> results/global_p$p"_"q$q/val/summary_results.txt;
+				echo $model_and_score >> results/global_p$p"_"q$q/test/summary_results.txt;
 
 			done
 		done
