c5155580a5dfeaab651522afa71f62dbef82517b
3cixty <cixty@garoupe.eurecom.fr>
2017-04-26 15:26:04 +0200

    modify node2vec implementation by using walk generator to speed up computation and keep low memory overhead

--- src/node2vec.py
+++ src/node2vec.py
@@ -1,21 +1,54 @@
+from __future__ import print_function
 import numpy as np
 import networkx as nx
 import random
 import gzip
+import time
+import argparse
+from gensim.models import Word2Vec
+from multiprocessing import Pool
+from chunks import chunks
 
-
-
-class Graph():
+class node2vec(object):
 	
-	def __init__(self, nx_G, is_directed, p, q, preprocessing):
-		self.G = nx_G
+	def __init__(self, is_directed, preprocessing, is_weighted, p, q, walk_length, num_walks, dimensions, window_size, workers, iterations):
+
+		#graph properties
 		self.is_directed = is_directed
+		self.preprocessing = preprocessing
+		self.is_weighted = is_weighted
+
+		#walk properties
 		self.p = p
 		self.q = q
-		self.preprocessing = preprocessing
+		self.walk_length = walk_length
+		self.num_walks = num_walks
+
+		#learning properties
+		self.dimensions = dimensions
+		self.window_size = window_size
+		self.workers = workers
+		self.iter = iterations
+
+	def read_graph(self, nx_G):
+
+		if self.is_weighted:
+
+			self.G = nx.read_edgelist(nx_G,  data=(('weight',float),), create_using=nx.DiGraph(), edgetype = str)
+
+		else:
+
+			self.G = nx.read_edgelist(nx_G, create_using=nx.DiGraph(), edgetype = str)
+
+			for edge in self.G.edges():
+				self.G[edge[0]][edge[1]]['weight'] = 1
+
+		if not self.is_directed:
+
+			self.G = self.G.to_undirected()
 
 
-	def node2vec_walk(self, walk_length, start_node):
+	def node2vec_walk(self, start_node):
 		'''
 		Simulate a random walk starting from start node.
 		'''
@@ -23,7 +56,7 @@ class Graph():
 
 		walk = [start_node]
 
-		while len(walk) < walk_length:
+		while len(walk) < self.walk_length:
 
 			cur = walk[-1]
 			cur_nbrs = sorted(G.neighbors(cur))
@@ -37,12 +70,12 @@ class Graph():
 
 					if len(walk) == 1: #first step of the walk, no previous node
 
-						walk.append(cur_nbrs[alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
+						walk.append(cur_nbrs[node2vec.alias_draw(alias_nodes[cur][0], alias_nodes[cur][1])])
 
 					else:
 						prev = walk[-2]
 
-						next_node = cur_nbrs[alias_draw(alias_edges[(prev, cur)][0], 
+						next_node = cur_nbrs[node2vec.alias_draw(alias_edges[(prev, cur)][0], 
 							alias_edges[(prev, cur)][1])]
 
 						walk.append(next_node)
@@ -96,37 +129,23 @@ class Graph():
 		return walk
 
 
-	def simulate_walks(self, num_walks, walk_length, output, p, q):
+
+	def learn_embeddings(self, output):
 		'''
-		Repeatedly simulate random walks from each node.
+		Learn embeddings by optimizing the Skipgram objective using SGD.
 		'''
-		G = self.G
 
-		nodes = G.nodes()
-		print 'Walk iteration:'
+		model = Word2Vec(self, size=self.dimensions, window=self.window_size, min_count=0, 
+			workers=self.workers, iter=self.iter, negative = 25, sg = 1)
 
-		with gzip.open('walks/%s' %output,'w') as walks_file:
-			for walk_iter in range(num_walks):
-				print str(walk_iter+1), '/', str(num_walks)
-				random.shuffle(nodes)
-				for node in nodes:
-					walk = self.node2vec_walk(walk_length=walk_length, start_node=node)
-					
-					c = 1
-					length = len(walk)
-					for entity in walk:
+		print("defined model using w2v")
 
-						if c == length:
-							
-							walks_file.write(''.join(entity.encode('utf-8')+'\n'))
+		model.wv.save_word2vec_format(output, binary = True)
 
-						else:
+		print("saved model in word2vec format")
 
-							walks_file.write(''.join(entity.encode('utf-8')))
-							walks_file.write(' ')
-							c += 1
+		return
 
-		return 
 
 	def get_alias_edge(self, src, dst):
 		'''
@@ -147,7 +166,26 @@ class Graph():
 		norm_const = sum(unnormalized_probs)
 		normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
 
-		return alias_setup(normalized_probs)
+		return node2vec.alias_setup(normalized_probs)
+
+
+	def __iter__(self):
+
+		'''
+		Generator of walks simulate random walks from each node.
+		'''
+		G = self.G
+		nodes = G.nodes()
+		print('Walk iteration:')
+
+		for walk_iter in range(self.num_walks):
+
+			print(str(walk_iter+1), '/', str(self.num_walks))
+			random.shuffle(nodes)
+
+			for node in nodes:
+
+				yield self.node2vec_walk(start_node=node)
 
 	def preprocess_transition_probs(self):
 		'''
@@ -161,7 +199,7 @@ class Graph():
 			unnormalized_probs = [G[node][nbr]['weight'] for nbr in sorted(G.neighbors(node))]
 			norm_const = sum(unnormalized_probs)
 			normalized_probs =  [float(u_prob)/norm_const for u_prob in unnormalized_probs]
-			alias_nodes[node] = alias_setup(normalized_probs)
+			alias_nodes[node] = node2vec.alias_setup(normalized_probs)
 
 		alias_edges = {}
 		triads = {}
@@ -174,56 +212,162 @@ class Graph():
 				alias_edges[edge] = self.get_alias_edge(edge[0], edge[1])
 				alias_edges[(edge[1], edge[0])] = self.get_alias_edge(edge[1], edge[0])
 
-		#print alias_nodes
-
-		#print alias_edges
-
 		self.alias_nodes = alias_nodes
 		self.alias_edges = alias_edges
 
 		return
 
+	@staticmethod
+	def alias_setup(probs):
+		'''
+		Compute utility lists for non-uniform sampling from discrete distributions.
+		Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
+		for details
+		'''
+		K = len(probs)
+		q = np.zeros(K)
+		J = np.zeros(K, dtype=np.int)
 
-def alias_setup(probs):
-	'''
-	Compute utility lists for non-uniform sampling from discrete distributions.
-	Refer to https://hips.seas.harvard.edu/blog/2013/03/03/the-alias-method-efficient-sampling-with-many-discrete-outcomes/
-	for details
-	'''
-	K = len(probs)
-	q = np.zeros(K)
-	J = np.zeros(K, dtype=np.int)
+		smaller = []
+		larger = []
+		for kk, prob in enumerate(probs):
+		    q[kk] = K*prob
+		    if q[kk] < 1.0:
+		        smaller.append(kk)
+		    else:
+		        larger.append(kk)
 
-	smaller = []
-	larger = []
-	for kk, prob in enumerate(probs):
-	    q[kk] = K*prob
-	    if q[kk] < 1.0:
-	        smaller.append(kk)
-	    else:
-	        larger.append(kk)
+		while len(smaller) > 0 and len(larger) > 0:
+		    small = smaller.pop()
+		    large = larger.pop()
 
-	while len(smaller) > 0 and len(larger) > 0:
-	    small = smaller.pop()
-	    large = larger.pop()
+		    J[small] = large
+		    q[large] = q[large] + q[small] - 1.0
+		    if q[large] < 1.0:
+		        smaller.append(large)
+		    else:
+		        larger.append(large)
 
-	    J[small] = large
-	    q[large] = q[large] + q[small] - 1.0
-	    if q[large] < 1.0:
-	        smaller.append(large)
-	    else:
-	        larger.append(large)
+		return J, q
 
-	return J, q
+	@staticmethod
+	def alias_draw(J, q):
+		'''
+		Draw sample from a non-uniform discrete distribution using alias sampling.
+		'''
+		K = len(J)
 
-def alias_draw(J, q):
-	'''
-	Draw sample from a non-uniform discrete distribution using alias sampling.
-	'''
-	K = len(J)
+		kk = int(np.floor(np.random.rand()*K))
+		if np.random.rand() < q[kk]:
+		    return kk
+		else:
+		    return J[kk]
 
-	kk = int(np.floor(np.random.rand()*K))
-	if np.random.rand() < q[kk]:
-	    return kk
-	else:
-	    return J[kk]
+
+	@staticmethod
+	def parse_args():
+		'''
+		Parses the node2vec arguments.
+		'''
+		parser = argparse.ArgumentParser(description="Run node2vec.")
+
+		parser.add_argument('--input', nargs='?', default='graph/karate.edgelist',
+		                    help='Input graph path')
+
+		parser.add_argument('--output', nargs='?', default='walks.txt.gz',
+	                   help='emb file name')
+
+		parser.add_argument('--walk_length', type=int, default=10,
+		                    help='Length of walk per source. Default is 10.')
+
+		parser.add_argument('--num_walks', type=int, default=500,
+		                    help='Number of walks per source. Default is 40.')
+
+		parser.add_argument('--p', type=float, default=1,
+		                    help='Return hyperparameter. Default is 1.')
+
+		parser.add_argument('--q', type=float, default=1,
+		                    help='Inout hyperparameter. Default is 1.')
+
+		parser.add_argument('--weighted', dest='weighted', action='store_true',
+		                    help='Boolean specifying (un)weighted. Default is unweighted.')
+		parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+		parser.set_defaults(weighted=False)
+
+		parser.add_argument('--directed', dest='directed', action='store_true',
+		                    help='Graph is (un)directed. Default is directed.')
+		parser.set_defaults(directed=False)
+
+		parser.add_argument('--no_preprocessing', dest = 'preprocessing', action='store_false',
+		                    help='Whether preprocess all transition probabilities or compute on the fly')
+		parser.set_defaults(preprocessing=True)
+
+		parser.add_argument('--dimensions', type=int, default=500,
+		                    help='Number of dimensions. Default is 128.')
+
+		parser.add_argument('--window-size', type=int, default=10,
+	                    	help='Context size for optimization. Default is 10.')
+
+		parser.add_argument('--iter', default=5, type=int,
+	                      help='Number of epochs in SGD')
+
+		parser.add_argument('--workers', type=int, default=8,
+		                    help='Number of parallel workers. Default is 8.')
+
+
+
+		return parser.parse_args()
+
+
+	def run(self, input_graph, output):
+
+		self.read_graph(input_graph)
+
+		if self.preprocessing:
+			self.preprocess_transition_probs()
+			print('preprocessed')
+
+		self.learn_embeddings(output)
+
+
+if __name__ == '__main__':
+
+	start_time = time.time()
+
+	args = node2vec.parse_args()
+
+	print('Parameters:\n')
+
+	print('input = %s\n' %args.input)
+
+	print('output = %s\n' %args.output)
+
+	print('walk length = %d\n' %args.walk_length)
+
+	print('number of walks per entity = %d\n' %args.num_walks)
+
+	print('p = %s\n' %args.p)
+
+	print('q = %s\n' %args.q)
+
+	print('weighted = %s\n' %args.weighted)
+
+	print('directed = %s\n' %args.directed)
+
+	print('no_preprocessing = %s\n' %args.preprocessing)
+
+	print('dimensions = %s\n' %args.dimensions)
+
+	print('iterations = %s\n' %args.iter)
+
+	print('window size = %s\n' %args.window_size)
+
+	print('dimensions = %s\n' %args.dimensions)
+
+	node2vec_graph = node2vec(args.directed, args.preprocessing, args.weighted, args.p, args.q, args.walk_length, args.num_walks, args.dimensions, args.window_size, args.workers, args.iter)
+
+	print('defined G')
+
+	node2vec_graph.run(args.input, args.output)
+
+	print("--- %s seconds ---" % (time.time() - start_time))
